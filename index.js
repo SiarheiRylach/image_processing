a8ba3af9691120811bda4429ee44ec0624b16d48
Siarhei Rylach <s.n.rylach@gmail.com>
2018-04-05 01:01:56 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var Jimp = require("jimp");
+
+Jimp.read("son-kot.jpg", function (err, image) {
+
+	image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+	    let red   = this.bitmap.data[ idx + 0 ];
+	    let green = this.bitmap.data[ idx + 1 ];
+	    let blue  = this.bitmap.data[ idx + 2 ];
+	    let alpha = this.bitmap.data[ idx + 3 ];
+	 
+	    let S = blue + green + red;
+
+	    if (S > ((255 / 2) * 3)){
+	    	this.bitmap.data[ idx + 0 ] = this.bitmap.data[ idx + 1 ] = this.bitmap.data[ idx + 2 ] = 255;
+	    }
+		else{
+			this.bitmap.data[ idx + 0 ] = this.bitmap.data[ idx + 1 ] = this.bitmap.data[ idx + 2 ] = 0;
+		}
+
+		if(x === image.bitmap.width-1 && y === image.bitmap.height-1) {
+        	image.write("2.jpg");
+    	} 		
+
+	});
+});
