a60094ae8346450f20992c54c2b6c7ac324c2072
Siarhei_Rylach <Siarhei_Rylach@epam.com>
2018-04-05 18:41:27 +0300

    noise

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const convertToBinary = require("./lib/binary");
 const convertToGrayscale = require("./lib/grayscale");
 const convertToSepia = require("./lib/sepia");
 const convertToNegative = require("./lib/negative");
-const noiseForPicture = require("./lib/noise");
+const addNoiseForPicture = require("./lib/noise");
+const changeBrightnessForPicture = require("./lib/brightness");
 
 //convertToBinary("4.jpg", "result.jpg");
 //convertToGrayscale("1.jpg", "result.jpg");
 //convertToSepia("1.jpg", "result.jpg");
 //convertToNegative("1.jpg", "result.jpg");
-noiseForPicture("1.jpg", "result.jpg");
+addNoiseForPicture("1.jpg", "result.jpg", -10);
+//changeBrightnessForPicture("1.jpg", "result.jpg", -20);
