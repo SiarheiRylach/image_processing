dedadf4c3c0984ab89ccb53a1826adfa3be0beb0
Siarhei_Rylach <Siarhei_Rylach@epam.com>
2018-04-05 18:21:38 +0300

    negative

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const convertToBinary = require("./lib/binary");
 const convertToGrayscale = require("./lib/grayscale");
 const convertToSepia = require("./lib/sepia");
+const convertToNegative = require("./lib/negative");
 
-convertToBinary("4.jpg", "result.jpg");
+//convertToBinary("4.jpg", "result.jpg");
 //convertToGrayscale("1.jpg", "result.jpg");
 //convertToSepia("1.jpg", "result.jpg");
+convertToNegative("1.jpg", "result.jpg");
